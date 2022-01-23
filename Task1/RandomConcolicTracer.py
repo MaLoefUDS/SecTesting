a4aceb389e17f5a70fa7896c37a08f0ea99c0e9a
Maximilian Löffler <s8maloef@stud.uni-saarland.de>
2022-01-23 23:20:32 +0100

    task1

--- Task1/RandomConcolicTracer.py
+++ Task1/RandomConcolicTracer.py
@@ -8,5 +8,15 @@ def Not(x: bool):
 class RandomConcolicTracer(ConcolicTracer):
     def reval(self, attempts: int):
         assert attempts > 0
+        for _ in range(attempts):
+            env = dict()
+            for key in self.decls.keys():
+                value = random_int() if self.decls[key] == "Int" else random_string()
+                locals()[key] = value
+                env[key] = value, self.decls[key]
+            sat = True
+            for statement in self.path:
+                sat = sat and eval(f"{statement}")
+            if sat:
+                return "sat", env
         return 'unsat', {}
-        # TODO: Implement me
