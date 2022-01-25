e1f1000348c838afda351c395ac6e550265148f2
Maximilian Löffler <s8maloef@stud.uni-saarland.de>
2022-01-25 22:03:37 +0100

    few helping methods

--- Task2/Summaries.py
+++ Task2/Summaries.py
@@ -1,3 +1,5 @@
+import string
+
 import z3
 import fuzzingbook.ConcolicFuzzer
 from fuzzingbook.ConcolicFuzzer import zstr, zint, zbool, fresh_name, ConcolicTracer
@@ -15,6 +17,35 @@ class zstr(zstr):
         find = self.find(m)
         return zbool(self.context, find.z != no_match.z, self.v.find(m) != -1)
 
+    def on_all(self, cond) -> zbool:
+        z3s = list()
+        pys = list()
+        for char in self:
+            z3_cond, py_cond = cond(char)
+            z3s.append(z3_cond)
+            pys.append(py_cond)
+        return zbool(self.context, z3.And(z3s), all(pys))
+
+    def in_range(self, start, end):
+        def check_include(char):
+            ordinal = zint(self.context, z3.IntVal(ord(char)), ord(char))
+            z3_cond = z3.And([(ordinal >= start).z, (ordinal <= end).z])
+            py_cond = start <= ordinal.v <= end
+            return z3_cond, py_cond
+        return self.on_all(check_include)
+
+    def in_set(self, _set):
+        def check_include(char):
+            ordinal = zint(self.context, z3.IntVal(ord(char)), ord(char))
+            z3s = list()
+            pys = list()
+            for elem in _set:
+                cond = (ordinal == elem)
+                z3s.append(cond.z)
+                pys.append(cond.v)
+            return z3.Or(z3s), any(pys)
+        return self.on_all(check_include)
+
     def capitalize(self) -> zstr:
         pass # TODO: Implement me
 
@@ -28,43 +59,35 @@ class zstr(zstr):
             return zbool(self.context, diff < 0, diff.v < 0)
         return zbool(self.context, self.startswith(other, beg=diff.v).z, self.v.startswith(other.v, diff.v))
 
-    def on_all(self, cond) -> zbool:
-        z3s = list()
-        pys = list()
-        for char in self:
-            z3_cond, py_cond = cond(char)
-            z3s.append(z3_cond)
-            pys.append(py_cond)
-        return zbool(self.context, z3.And(z3s), all(pys))
-
     def isalnum(self) -> zbool:
-        pass # TODO: Implement me
+        is_dec = self.isdecimal()
+        is_dig = self.isdigit()
+        is_num = self.isnumeric()
+        return zbool(self.context,
+                     z3.Or([is_dec.z, is_dig.z, is_num.z]),
+                     any([is_dec.v, is_dig.v, is_num.v]))
 
     def isdecimal(self) -> zbool:
-        pass # TODO: Implement me
+        return self.in_range(ord("0"), ord("9"))
 
     def isdigit(self) -> zbool:
-        pass # TODO: Implement me
+        pass  # TODO: Implement me
 
     def islower(self) -> zbool:
-        pass # TODO: Implement me
+        return self.in_range(ord("a"), ord("z"))
 
     def isnumeric(self) -> zbool:
         pass # TODO: Implement me
 
     def isprintable(self) -> zbool:
-        pass # TODO: Implement me
+        return self.in_set(string.printable)
 
     def isspace(self) -> zbool:
+        _set = [" ", "\t", "\n"]
         pass # TODO: Implement me
 
     def isupper(self) -> zbool:
-        def upper_check(char):
-            ordinal = zint(self.context, z3.IntVal(ord(char)), ord(char))
-            z3_cond = z3.And([(ordinal >= ord("A")).z, (ordinal <= ord("Z")).z])
-            py_cond = ord("A") <= ordinal.v <= ord("Z")
-            return z3_cond, py_cond
-        return self.on_all(upper_check)
+        return self.in_range(ord("A"), ord("Z"))
 
     def rfind(self, sub: str, start: int = None, stop: int = None) -> zint:
         assert start is None, 'No need to handle this parameter'
@@ -91,11 +114,13 @@ if __name__ == "__main__":
         string2 = zstr(_.context, z3.String("string_2"), "World")
         string3 = zstr(_.context, z3.String("string_3"), "Work")
         string4 = zstr(_.context, z3.String("string_4"), "WORK")
+        empty = zstr(_.context, z3.String("empty"), " ")
         assert "Hello" in string1, "__contains__ failed"
         assert "Help" not in string1, "__contains__ failed"
         assert string1.endswith(string2, None, None), "endswith failed"
         assert not string1.endswith(string3, None, None), "endswith failed"
         assert not string3.isupper(), "isupper failed"
+        assert not empty.isupper(), "isupper failed"
         assert string4.isupper(), "isupper failed"
         print("passed all tests")
 
